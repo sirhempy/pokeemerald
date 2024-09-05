cafd6b94a98b2026f4ac7c4e1444ae03ccebfd50
sirhempy <32631596+sirhempy@users.noreply.github.com>
2024-09-05 11:50:40 -0700

    Updated w/
    -Press R while on clock screen to change time [Credit: paccy]
    - Disabled Match Calls not related to rematches [Credit: Devolov]
    -Dex entries not shown w/o receiving dex [Credit: AsparagusEduardo]
    -Configs: Save block, gen 5 OW popups, held item evos, level cap implement

--- src/level_caps.c
+++ src/level_caps.c
@@ -9,15 +9,15 @@ u32 GetCurrentLevelCap(void)
 {
     static const u32 sLevelCapFlagMap[][2] =
     {
-        {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
-        {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
-        {FLAG_BADGE07_GET, 42},
-        {FLAG_BADGE08_GET, 46},
-        {FLAG_IS_CHAMPION, 58},
+        {FLAG_BADGE01_GET, 20},
+        {FLAG_BADGE02_GET, 26},
+        {FLAG_BADGE03_GET, 32},
+        {FLAG_BADGE04_GET, 37},
+        {FLAG_BADGE05_GET, 46},
+        {FLAG_BADGE06_GET, 47},
+        {FLAG_BADGE07_GET, 50},
+        {FLAG_BADGE08_GET, 62},
+        {FLAG_IS_CHAMPION, 100},
     };
 
     u32 i;
